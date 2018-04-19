dc5762b4368009b21df01f0957a5586a61ce2be7
Wiradani Ramadhan <adhan432@gmail.com>
2018-04-19 17:35:10 +0700

    reward page input

---
+++ src/pages/modal-reward/modal-reward.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { ModalRewardPage } from './modal-reward';
+
+@NgModule({
+  declarations: [
+    ModalRewardPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(ModalRewardPage),
+  ],
+})
+export class ModalRewardPageModule {}
